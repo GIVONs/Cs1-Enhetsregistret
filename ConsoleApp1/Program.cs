04ab34fbb25d04bc610c9c5b288e83892b94b295
GIVONs <givon.r.olea@gmail.com>
2026-09-08 11:45:47 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿string enhet1 = "Laptopsal 1";
+string id1 = "D-100";
+string status1 = "Aktiv";
+int price1 = 250;
+
+string enhet2 = "Laptopsal 1";
+string id2 = "D-200";
+string status2 = "Aktiv";
+int price2 = 100;
+
+string enhet3 = "Laptopsal 1";
+string id3 = "D-300";
+string status3 = "Aktiv";
+int price3 = 800;
+
+// Rad 2 i vår register 
+string row2 = "ID "
+    + id2 + " | "
+    + enhet2 + " | "
+    + price2
+    + "Status: "
+    + status2;
+
+
+Console.WriteLine("Emnhetsregistret:");
+Console.WriteLine("=======================");
+
+Console.WriteLine($"\nID-100 | Namn : {enhet1} | Price: {price1} | Status : {status1.ToString()}");
+Console.WriteLine($"ID-200 | Namn : {enhet2} | Price: {price2} | Status : {status2.ToString()}");
+Console.WriteLine($"ID-300 | Namn : {enhet3} | Price: {price3} | Status : {status3.ToString()}");
+
+Console.WriteLine("\n=======================");
+
+Console.WriteLine("2 enheter registerarde");
